8def31e5a1025b982ba0b23a64b27d1ab65a6c3f
alex-ele-xela <alex94prateek@gmail.com>
2020-08-25 21:24:12 +0530

    Updates

---
+++ precache-manifest.847eaec610564f85877a359c634c11cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73a429c643e0b444fb0ea9410ea1c4c",
+    "url": "/app-communicate/index.html"
+  },
+  {
+    "revision": "f30a8850fc418105ed71",
+    "url": "/app-communicate/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8a5b7aee87d73898c608",
+    "url": "/app-communicate/static/css/main.5e1c0852.chunk.css"
+  },
+  {
+    "revision": "f30a8850fc418105ed71",
+    "url": "/app-communicate/static/js/2.414e37b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-communicate/static/js/2.414e37b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5b7aee87d73898c608",
+    "url": "/app-communicate/static/js/main.3e076f08.chunk.js"
+  },
+  {
+    "revision": "75f5e9466ca7d0e3ea93",
+    "url": "/app-communicate/static/js/runtime-main.ce340322.js"
+  },
+  {
+    "revision": "e9326a81acb77382a61a1fbef182278f",
+    "url": "/app-communicate/static/media/logo.e9326a81.png"
+  }
+]);
